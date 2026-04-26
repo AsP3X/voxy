ccef234cdccd34e1c04a9c9635dbfeb1a21822ad
Niklas Vorberg <services@vorberg-webmail.de>
2026-04-26 15:12:30 +0200

    neoforge(1.21.1): fix MixinDefaultChunkRenderer @Inject target for Sodium 0.6.x
    Update ShaderChunkRenderer package path from me.jellysquid to net.caffeinemc to match Sodium 0.6.13

--- src/main/java/me/cortex/voxy/client/mixin/sodium/MixinDefaultChunkRenderer.java
+++ src/main/java/me/cortex/voxy/client/mixin/sodium/MixinDefaultChunkRenderer.java
@@ -40,7 +40,7 @@ public abstract class MixinDefaultChunkRenderer extends ShaderChunkRenderer {
         }
     }
 
-    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/render/chunk/ShaderChunkRenderer;end(Lme/jellysquid/mods/sodium/client/render/chunk/terrain/TerrainRenderPass;)V", shift = At.Shift.BEFORE))
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/caffeinemc/mods/sodium/client/render/chunk/ShaderChunkRenderer;end(Lnet/caffeinemc/mods/sodium/client/render/chunk/terrain/TerrainRenderPass;)V", shift = At.Shift.BEFORE))
     private void injectRender(ChunkRenderMatrices matrices, CommandList commandList, ChunkRenderListIterable renderLists, TerrainRenderPass renderPass, CameraTransform camera, CallbackInfo ci) {
         this.doRender(matrices, renderPass, camera);
     }
